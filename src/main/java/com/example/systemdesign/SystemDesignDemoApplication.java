bcd11e35b62641e1340c29a9f19b0fe04c2b181d
Yogesh Deshmukh <yogeshdeshmukh@Yogeshs-MacBook-Air.local>
2022-03-07 12:22:00 +0530

    initial commit

---
+++ src/main/java/com/example/systemdesign/SystemDesignDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.systemdesign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemDesignDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemDesignDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/systemdesign/SystemDesignDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.systemdesign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemDesignDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
